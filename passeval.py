7444676cc591167a6d182c3ed2449850592c7855
DiracSpace <deleon.martinez.16138@itsmante.edu.mx>
2020-05-21 00:22:12 -0400

    Fixed Regex, fixed error solutions, started with password time calculations

--- passeval.py
+++ passeval.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import requests, re, json, base64, querypass
+import requests, re, json, base64, querypass, passgen
 from pathlib import Path
 
 json = json.loads(open('links.json').read())
@@ -16,18 +16,29 @@ def geturl(key):
 
 def getList(pwd):
     try:
-        print ('\n')
-        answer = input('Do you want to give a custom filename? (y/N) -> ')
-        if answer == 'y':
-            filename = input('Input -> ')
+        filename = Path(geturl("path"))
+        if filename.exists():
+            print (f'Aparently, you do have the file at -> {filename}, using this')
+            parseText(pwd)
         else:
-            file = 'default'
-        url = geturl("git")
-        querypass.requestLib(url, filename)
-        parseText(pwd)
-        print ('\n')
+            print ('\n')
+            answer = input('Do you want to give a custom filename? (y/N) -> ')
+            if answer == 'y':
+                filename = input('Input -> ')
+            else:
+                filename = 'default'
+            url = geturl("git")
+            querypass.requestLib(url, filename)
+            parseText(pwd)
+            print ('\n')
     except Exception as e:
         print (f'Error getting list -> {e}')
+        if e == str(geturl("err")):
+            print ('Trying with cURL .. ')
+            querypass.cURL(url, 'default')
+        else:
+            # Check to see if I can change IP
+            print ('Got an error, try changing IP')
 
 def findCombinations(pwd, filename):
     combinations = []
@@ -41,6 +52,7 @@ def findCombinations(pwd, filename):
     for index, combination in enumerate(combinations):
         print (f'{index},   {combination}')
     print ('\n')
+    passgen.passTimeDefiner(pwd)
 
 def parseText(pwd):
     try:
@@ -61,18 +73,29 @@ def parseText(pwd):
         exit()
 
 def searchList(pwd):
-    list = []
-    combinations = []
-    pattern = re.compile(f'.*{pwd}.*')
-    url = geturl("git")
-    response = requests.get(url)
-    if response:
-        print (f'These are similarities or combinations with {pwd}')
-        result = pattern.findall(response.text)
-        for data in result:
-            combinations.append(data)
-        for index, combination in enumerate(combinations):
-            print (f'{index},   {combination}')
-        print ('\n')
-    else:
-        print ('errr')
+    try:
+        list = []
+        combinations = []
+        pattern = re.compile(f'.*{pwd}.*')
+        url = geturl("git")
+        response = requests.get(url)
+        if response:
+            print (f'These are similarities or combinations with {pwd}')
+            result = pattern.findall(response.text)
+            for data in result:
+                combinations.append(data)
+            for index, combination in enumerate(combinations):
+                print (f'{index},   {combination}')
+            print ('\n')
+            passgen.passTimeDefiner(pwd)
+        else:
+            print ('Error')
+            exit()
+    except Exception as e:
+        print (f'Searching problem -> {e}')
+        if e == str(geturl("err")):
+            print ('Trying with cURL .. ')
+            querypass.cURL(url, 'default')
+        else:
+            # Check to see if I can change IP
+            print ('Got an error, try changing IP')
--- passgen.py
+++ passgen.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-import string, random, sys
+import string, random, sys, passeval, re, requests
 
 r = random.SystemRandom()
+
+bad = '\033[91m[-]\033[0m'
+info = '\033[93m[!]\033[0m'
+good = '\033[92m[+]\033[0m'
 
 def generatePassword():
     if len(sys.argv) > 1:
@@ -17,3 +21,48 @@ def generatePassword():
             generatedpassword += r.choice(charset)
 
     return generatedpassword
+
+def printCool(numbers):
+    if numbers[1] >= '17':
+        print (f'%s Your password is {numbers[0]} characters long' % info)
+        print (f'%s With a total of {numbers[1]} combinations' % info)
+        print (f'%s It will take {numbers[2]} hours or {numbers[3]} days to brute force your password' % info)
+        print (f'%s A computer that trys {numbers[4]} passwords per hour' % info)
+    elif numbers[1] > '20':
+        print (f'%s Your password is {numbers[0]} characters long' % good)
+        print (f'%s With a total of {numbers[1]} combinations' % good)
+        print (f'%s It will take {numbers[2]} hours or {numbers[3]} days to brute force your password' % good)
+        print (f'%s A computer that trys {numbers[4]} passwords per hour' % good)
+    elif numbers[1] < '10':
+        print (f'%s Your password is {numbers[0]} characters long' % bad)
+        print (f'%s With a total of {numbers[1]} combinations' % bad)
+        print (f'%s It will take {numbers[2]} hours or {numbers[3]} days to brute force your password' % bad)
+        print (f'%s A computer that trys {numbers[4]} passwords per hour' % bad)
+
+def passTimeDefiner(pwd):
+    p = re.compile('<p>(.*?)</p>')
+    b = re.compile('<b>(.*?)</b>')
+    special = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+    try:
+        uc = sum(1 for c in pwd if c.isupper())
+        lc = sum(1 for c in pwd if c.islower())
+        nu = sum(c.isdigit() for c in pwd)
+        sc = special.findall(pwd)
+        sc = len(sc)
+        ran = 0
+        ransc = 0
+        dt = 0
+        #url = passeval.geturl("eval")
+        url = f'https://tmedweb.tulane.edu/content_open/bfcalc.php?uc={uc}&lc={lc}&nu={nu}&sc={sc}&ran={ran}&rans={ransc}&dict={dt}'
+        response = requests.get(url)
+        if response:
+            numbers = b.findall(str(p.findall(response.text)))
+            printCool(numbers)
+        else:
+            print ("Didn't get a response")
+            exit()
+        print ('\n')
+    except Exception as e:
+        print (f'Password strength definer problem -> {e}')
+        print ('\n')
+        exit()
